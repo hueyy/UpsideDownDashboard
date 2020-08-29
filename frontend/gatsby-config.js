da3e92554a57f7c48ae08e950be8960c97ea0738
Huey <hello@huey.xyz>
2020-08-29 19:05:18 +0800

    ☀ add PWA manifest

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -2,6 +2,18 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-scss-typescript`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Solar Dashboard`,
+        short_name: `Solar`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `standalone`,
+        icon: `src/assets/sun.png`
+      },
+    },
   ],
   siteMetadata: {
     title: `UpsideDown Dashboard`
